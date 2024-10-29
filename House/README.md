0af40e1e22d376824633df9d9f5a05c0d257618c
Varsha <varsha8150@gmail.com>
2024-10-29 17:06:08 +0530

    Add service

--- House/README.md
+++ House/README.md
@@ -5,3 +5,5 @@
 - ng new project-name
 - ng serve
 - ng generate component home --standalone --inline-style --inline-template --prefix app
+- ng generate interface housinglocation
+- ng generate service housing --skip-tests
--- House/src/app/housing-location/housing-location.component.ts
+++ House/src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import {HousingLocation} from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
+
